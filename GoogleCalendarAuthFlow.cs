5f5997bd5cad0ce3fffebe7213de21ae0efeb8fa
mbg <michael.gale@syngate.co.uk>
2015-08-02 08:59:23 +0100

    Initial code commit

---
+++ GoogleCalendarAuthFlow.cs
@@ -0,0 +1,128 @@
+﻿using Etilic.Identity.OAuth;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Auth.OAuth2.Requests;
+using Google.Apis.Auth.OAuth2.Responses;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Requests;
+using Google.Apis.Util;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Etilic.DC.GoogleCalendar
+{
+    /// <summary>
+    /// Implements the <see cref="Etilic.Identity.OAuth.IOAuthFlow"/> interface for Google Calendar.
+    /// </summary>
+    public class GoogleCalendarAuthFlow : IOAuthFlow
+    {
+        #region Instance members
+        /// <summary>
+        /// The Google Calendar bundle this OAuth flow belongs to.
+        /// </summary>
+        private GoogleCalendarBundle bundle;
+        /// <summary>
+        /// The configuration of the OAuth flow.
+        /// </summary>
+        private IAuthorizationCodeFlow flow;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets the globally unique ID of the service this OAuth flow is for.
+        /// </summary>
+        public Guid ServiceID
+        {
+            get { return GoogleCalendarBundle.ID; }
+        }
+
+        public IAuthorizationCodeFlow Flow
+        {
+            get { return this.flow; }
+        }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Initialises a new <see cref="Etilic.DC.GoogleCalendar.GoogleCalendarAuthFlow"/> object.
+        /// </summary>
+        /// <param name="bundle">
+        /// The <see cref="Etilic.DC.GoogleCalendar.GoogleCalendarBundle"/> which created this object.
+        /// </param>
+        public GoogleCalendarAuthFlow(GoogleCalendarBundle bundle)
+        {
+            // bundle can't be null
+            if (bundle == null)
+                throw new ArgumentNullException("bundle");
+
+            this.bundle = bundle;
+
+            // configure the OAuth flow
+            this.flow = new GoogleAuthorizationCodeFlow(new GoogleAuthorizationCodeFlow.Initializer
+            {
+                ClientSecrets = new ClientSecrets 
+                {
+                    ClientId = this.bundle.Configuration["GoogleClientID"],
+                    ClientSecret = this.bundle.Configuration["GoogleClientSecret"]
+                },
+                Scopes = new[] { CalendarService.Scope.Calendar }
+            });
+        }
+        #endregion
+
+        #region GetRequestUri
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="immediateCallback"></param>
+        /// <param name="callbackParam"></param>
+        /// <returns></returns>
+        public Uri GetRequestUri(String immediateCallback, Guid callbackParam)
+        {
+            // create an OAuth request
+            AuthorizationCodeRequestUrl request = this.flow.CreateAuthorizationCodeRequest(immediateCallback);
+
+            // return the Uri for the request
+            return request.Build();
+        }
+        #endregion
+
+        #region ExchangeCode
+        /// <summary>
+        /// Exchanges an authentication code for an authentication token.
+        /// </summary>
+        /// <param name="previousCallback"></param>
+        /// <param name="code"></param>
+        /// <returns></returns>
+        public OAuthToken ExchangeCode(String previousCallback, String userID, String code)
+        {
+            Task<TokenResponse> response = Task.Run<TokenResponse>(async () =>
+            {
+                return await this.flow.ExchangeCodeForTokenAsync(
+                    userID, 
+                    code, 
+                    previousCallback, 
+                    CancellationToken.None);
+            });
+            response.Wait();
+
+            TokenResponse result = response.Result;
+
+            OAuthToken token = new OAuthToken(this.ServiceID);
+            token.AccessToken = result.AccessToken;
+            token.RefreshToken = result.RefreshToken;
+            token.Scope = result.Scope;
+            token.Issued = DateTime.UtcNow;
+            token.Expires = token.Issued.AddSeconds((double)result.ExpiresInSeconds);
+
+            // return the OAuth token
+            return token;
+        }
+        #endregion
+    }
+}
---
+++ GoogleCalendarBundle.cs
@@ -0,0 +1,103 @@
+﻿using Etilic.Core.Extensibility;
+using Etilic.Identity;
+using Etilic.Identity.OAuth;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Auth.OAuth2.Responses;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Etilic.DC.GoogleCalendar
+{
+    /// <summary>
+    /// Google Calendar bundle for Etilic.
+    /// </summary>
+    public class GoogleCalendarBundle : Bundle
+    {
+        #region Constants
+        /// <summary>
+        /// The globally unique ID of this bundle.
+        /// </summary>
+        public const String GUID = "6DCAD5D3-6451-4B9F-AFFA-5AD9D3EAC4D2";
+        #endregion
+
+        #region Static Properties
+        /// <summary>
+        /// The globally unique ID of this bundle.
+        /// </summary>
+        public static Guid ID = new Guid(GUID);
+        #endregion
+
+        private GoogleCalendarAuthFlow flow;
+
+        #region Properties
+        /// <summary>
+        /// Gets the globally unique ID of this bundle.
+        /// </summary>
+        public override Guid BundleID
+        {
+            get { return ID; }
+        }
+        /// <summary>
+        /// Gets the globally unique IDs of this bundle's dependencies.
+        /// </summary>
+        public override Guid[] Dependencies
+        {
+            get
+            {
+                return new Guid[] {  
+                    DCBundle.ID,
+                    IdentityBundle.ID
+                };
+            }
+        }
+        #endregion
+
+        #region OnAdded
+        /// <summary>
+        /// Invoked by <see cref="Etilic.Core.Extensibility.BundleManager"/> after the bundle
+        /// has been registered.
+        /// </summary>
+        public override void OnAdded()
+        {
+            // get the identity bundle and register the Google Calendar OAuth flow
+            var identityBundle = BundleManager.GetBundle<IdentityBundle>(IdentityBundle.ID);
+
+            this.flow = new GoogleCalendarAuthFlow(this);
+            identityBundle.RegisterOAuthFlow(this.flow);
+        }
+        #endregion
+
+        #region CreateService
+        /// <summary>
+        /// Creates a <see cref="Google.Apis.v3.CalendarService"/> object for the given user
+        /// and OAuth token.
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public CalendarService CreateService(String userID, OAuthToken token)
+        {
+            // re-construct the TokenResponse object from our OAuthToken object
+            // note that Google's API will use local time to verify that the
+            // access token is still valid, so we need to convert it to that
+            TokenResponse response = new TokenResponse();
+            response.AccessToken = token.AccessToken;
+            response.Issued = token.Issued.ToLocalTime();
+            response.ExpiresInSeconds = (long)token.Expires.Subtract(token.Issued).TotalSeconds;
+            response.TokenType = token.TokenType;
+
+            BaseClientService.Initializer initialiser = new BaseClientService.Initializer();
+            initialiser.HttpClientInitializer = new UserCredential(this.flow.Flow, userID, response);
+
+            CalendarService service = new CalendarService(initialiser);
+
+            return service;
+        }
+        #endregion
+    }
+}
